fb644773e187e35e49d5ac7b599f6b9c97b4f481
Replit user <>
2023-08-18 14:35:26 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -114,11 +114,31 @@ void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2,
 
 /*
 Ejercicio 5: Comprobación de Ordenación
-Descripción: Escribe una función que tome un arreglo y su tamaño,
+Descripción:Escribe una función que tome un arreglo y su tamaño,
 y luego devuelva 1 si el arreglo está ordenado en orden ascendente,
   0 si no está ordenado, y -1 si está ordenado en orden descendente.
 */
-int checkSorted(int arr[], int size) { return -2; }
+int checkSorted(int arr[], int size) {
+      int ascendente = 1; 
+    int descendente = 1; 
+
+    for (int i = 1; i < size; i++) {
+        if (arr[i - 1] > arr[i]) {
+            ascendente = 0;
+        }
+        if (arr[i - 1] < arr[i]) {
+            descendente = 0; 
+        }
+    }
+
+    if (ascendente) {
+        return 1; 
+    } else if (descendente) {
+        return -1;
+    } else {
+        return 0; 
+    }
+}
 
 /*
 Ejercicio 6: Información de una Biblioteca
